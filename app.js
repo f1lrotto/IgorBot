89b0e77e78f0245798f6e2fbfb3045cc1745a434
Filip Hupka <77974144+f1lrotto@users.noreply.github.com>
2023-09-23 00:10:35 +0200

    add endpoints

--- app.js
+++ app.js
@@ -21,6 +21,26 @@ function startApp() {
   console.log(`Server listening on port ${PORT}`);
 }
 
+// Endpoint to run the news scraper
+app.get('/runNewsScraper', async (req, res) => {
+  try {
+    await controller.runNewsScraper();
+    res.status(200).send('News scraper executed successfully.');
+  } catch (error) {
+    res.status(500).send('Failed to execute news scraper: ' + error.message);
+  }
+});
+
+// Endpoint to send the news
+app.get('/sendNews', async (req, res) => {
+  try {
+    await controller.sendNews();
+    res.status(200).send('News sent successfully.');
+  } catch (error) {
+    res.status(500).send('Failed to send news: ' + error.message);
+  }
+});
+
 // Start the MongoDB connection
 connectMongo()
   .then(() => {
