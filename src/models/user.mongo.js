975e557999c61b7f1ffcbe72397286849ecbf9dd
Filip Hupka <filip.hupka@gmail.com>
2024-04-30 01:56:53 +0200

    Add a user activity schema to set up the 'server stats' feture

---
+++ src/models/user.mongo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userActivitySchema = new mongoose.Schema({
+  userId: { type: String, required: true },
+  guildId: { type: String, required: true },
+  totalMessages: { type: Number, default: 0 },
+  messagesPerChannel: [{
+    channelId: String,
+    count: { type: Number, default: 0 }
+  }],
+  totalVoiceTime: { type: Number, default: 0 }, // total voice time in milliseconds
+});
+
+module.exports = mongoose.model('UserActivity', userActivitySchema);
