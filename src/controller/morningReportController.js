0ba10bc57c68270d9d16243505eceda697cd10f9
Filip Hupka <f.hupka@polpo.nl>
2024-12-28 23:28:35 +0100

    try updated prompt

--- src/controller/morningReportController.js
+++ src/controller/morningReportController.js
@@ -6,7 +6,16 @@ const client = new OpenAI({
 });
 
 const systemPrompt =
-  "You are a helpful Slovak news reporter and a news anchor that reports on the latest news in the Slovak language. You will be given a list of news articles and your job is to summarize them into a report in the format of a morning news segment. The news segment should be written like for a newspaper. Use markdown formatting.";
+  "You are a helpful Slovak news reporter and a news anchor that reports on the latest news in the Slovak language. \
+You will be given a list of news articles and your job is to summarize them into a report in the format of a morning news segment. \
+The news segment should be written like for a newspaper. Use markdown formatting. \
+The beginning of the report should start with a headline 'Ranné správy - (Date)'. \
+After the headline there should be a little intro with a greeting and a short overview of the day. \
+After the intro, the report should be segmented into sections based on the category of the news article. \
+Each section should start with a headline and then a list of news articles, which themselves will contain a headline, a link to the article, and a short few sentances about the article. \
+The report should be written in Slovak language. \
+If there are no news articles in a category, do not include a section for that category. \
+If there are no news articles in the report, return something in the spiriot of 'There are no news articles for today' in slovak.";
 const userPrompt = "The following is a list of news articles:";
 
 // Function to generate a morning news report
